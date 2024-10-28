3783bae0216f59f18386a646d14ad912b56d16c3
Harry <ddembrs@gmail.com>
2024-10-28 16:30:03 +0800

    backup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,16 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   // 模塊注入
   modules: [// https://nuxt.com.cn/modules/element-plus
-    '@nuxtjs/stylelint-module' // https://color-mode.nuxtjs.org/
-    // '@element-plus/nuxt', // https://ithelp.ithome.com.tw/articles/10302381
-    // '@pinia/nuxt', // const {} = useDevice(); // https://nuxt.com/modules/device
-    // '@nuxtjs/device', // https://github.com/nuxt-modules/stylelint
-    // '@nuxtjs/color-mode', // https://nuxt.com/modules/icon
-    // 'nuxt-icon', // https://google-fonts.nuxtjs.org/getting-started/setup
-    // '@nuxtjs/google-fonts', // https://nuxt.com/modules/unocss
-    // '@unocss/nuxt',
-    // '@nuxt/image'
+  // https://color-mode.nuxtjs.org/
+    '@nuxtjs/stylelint-module', // const {} = useDevice(); // https://nuxt.com/modules/device
+    '@pinia/nuxt', // https://pinia.vuejs.org/ssr/nuxt.html
+    '@nuxt/icon' // https://google-fonts.nuxtjs.org/getting-started/setup
+  // '@element-plus/nuxt', // https://ithelp.ithome.com.tw/articles/10302381
+  // '@nuxtjs/device', // https://github.com/nuxt-modules/stylelint
+  // '@nuxtjs/color-mode', // https://nuxt.com/modules/icon
+  // '@nuxtjs/google-fonts', // https://nuxt.com/modules/unocss
+  // '@unocss/nuxt',
+  // '@nuxt/image'
   ],
   devServer: {
     port: Number(process.env.NUXT_PORT || 3001),
