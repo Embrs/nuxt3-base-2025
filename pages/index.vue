31e87cdee6590c6b50a53b2288a30c8fde8e95d9
Harry <ddembrs@gmail.com>
2024-10-29 09:30:51 +0800

    ok

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// home 請填寫功能描述👈
+</script>
+
+<template lang="pug">
+.home
+  p home
+</template>
+
+<style lang="scss" scoped>
+// 佈局 ----
+.home {
+  // TODO
+}
+
+// 組件 ----
+</style>
