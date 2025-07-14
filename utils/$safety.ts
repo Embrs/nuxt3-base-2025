640b83c8c7d49a2e4a8615df07c91d5c80cd1a66
Harry <ddembrs@gmail.com>
2025-07-14 12:13:01 +0800

    backup

--- utils/$safety.ts
+++ utils/$safety.ts
@@ -1,4 +1,3 @@
-import CryptoJS from 'crypto-js';
 // ----------------------------------------------------------------------------------------
 const Base64Encode = (str: string) => {
   try {
@@ -30,53 +29,10 @@ const Base64Decode = (str: string) => {
   }
 };
 
-// -----------------------------------------------------------------------------------------
-const CreateKeyIV = (length: number = 16) => {
-  const randomWords = CryptoJS.lib.WordArray.random(length);
-  return CryptoJS.enc.Hex.stringify(randomWords);
-};
-
-let key: any = '';
-let iv: any = '';
-
-const InitKeyIv = () => {
-  const storeEnv = StoreEnv();
-  const _key = (storeEnv?.env?.aesKey as string) || '';
-  const _iv = (storeEnv?.env?.aesIv as string) || '';
-  key = CryptoJS.enc.Utf8.parse(_key);// 自訂 key
-  iv = CryptoJS.enc.Utf8.parse(_iv);// 自訂 iv
-};
-
-const AES256Encode = (any: any): string => {
-  try {
-    if (!key || !iv) InitKeyIv();
-    const str = JSON.stringify(any);
-    const encode = CryptoJS.AES.encrypt(str, key, { iv }).toString();
-    return encode;
-  } catch (error) {
-    return '';
-  }
-};
-
-const AES256Decode = (encode: string): any => {
-  try {
-    if (!key || !iv) InitKeyIv();
-    const str = CryptoJS.AES.decrypt(encode, key, { iv }).toString(CryptoJS.enc.Utf8);
-    const decode = JSON?.parse(str);
-    return decode;
-  } catch (error) {
-    return '';
-  }
-};
 // =========================================================================================
 export default {
   base64: {
     Encode: Base64Encode,
     Decode: Base64Decode
-  },
-  aes256: {
-    CreateKeyIV,
-    Encode: AES256Encode,
-    Decode: AES256Decode
   }
 };
