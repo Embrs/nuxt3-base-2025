611a3ce380ca6bd7c5909f35949384c1703c41a0
Harry <ddembrs@gmail.com>
2025-09-06 11:52:10 +0800

    fix: use verify

--- composables/tool/use-verify.ts
+++ composables/tool/use-verify.ts
@@ -1,11 +1,20 @@
 // Form 驗證集合
+// https://github.com/yiminghe/async-validator
 export const UseVerify = () => {
-  const { t } = useI18n();
-  const enter = computed(() => ({ required: true, message: t('ph.enter'), trigger: 'change' }));
-  const select = computed(() => ({ required: true, message: t('ph.select'), trigger: 'change' }));
-
+  const enter = computed(() => ({ required: true, message: '請輸入', trigger: 'change' }));
+  const select = computed(() => ({ required: true, message: '請選擇', trigger: 'change' }));
+  const string = computed(() => ({ required: true, message: '請輸入', type: 'string', trigger: 'change' }));
+  const boolean = computed(() => ({ required: true, message: '請選擇', type: 'boolean', trigger: 'change' }));
+  const number = computed(() => ({ required: true, message: '請輸入數字', type: 'number', trigger: 'change' }));
+  const numberText = computed(() => ({ required: true, message: '請輸入數字', pattern: /^\d+$/, trigger: 'change' }));
+  const mail = computed(() => ({ required: true, message: '請輸入電子郵件', type: 'email', trigger: 'change' }));
   return {
     enter,
-    select
+    select,
+    string,
+    boolean,
+    number,
+    numberText,
+    mail
   };
 };
